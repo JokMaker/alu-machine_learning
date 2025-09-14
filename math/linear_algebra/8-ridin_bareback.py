4464fcc3eab775f6c64f94f5056408ce1982505c
JokMaker <j.kur@alustudent.com>
2025-09-14 18:53:22 +0200

    Fix pycodestyle E501: break long line in 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -16,4 +16,6 @@ def mat_mul(mat1, mat2):
     """
     if len(mat1[0]) != len(mat2):
         return None
-    return [[sum(mat1[i][k] * mat2[k][j] for k in range(len(mat2))) for j in range(len(mat2[0]))] for i in range(len(mat1))]
+    return [[sum(mat1[i][k] * mat2[k][j] for k in range(len(mat2)))
+             for j in range(len(mat2[0]))]
+            for i in range(len(mat1))]
