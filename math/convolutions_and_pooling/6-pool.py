54e237fc96b1d9197d27028f5b46fae88f5318a7
JokMaker <j.kur@alustudent.com>
2025-10-26 22:07:10 +0200

    Fix pycodestyle errors in 6-pool.py

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -18,17 +18,19 @@ def pool(images, kernel_shape, stride, mode='max'):
     m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
-    
+
     output_h = (h - kh) // sh + 1
     output_w = (w - kw) // sw + 1
-    
+
     pooled = np.zeros((m, output_h, output_w, c))
-    
+
     for i in range(output_h):
         for j in range(output_w):
             if mode == 'max':
-                pooled[:, i, j, :] = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :].max(axis=(1, 2))
+                pooled[:, i, j, :] = images[:, i*sh:i*sh+kh,
+                                            j*sw:j*sw+kw, :].max(axis=(1, 2))
             elif mode == 'avg':
-                pooled[:, i, j, :] = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :].mean(axis=(1, 2))
-    
+                pooled[:, i, j, :] = images[:, i*sh:i*sh+kh,
+                                            j*sw:j*sw+kw, :].mean(axis=(1, 2))
+
     return pooled
