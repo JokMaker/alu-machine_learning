5876483bb82c60a97bd676bfbe0f6f6e44688774
JokMaker <j.kur@alustudent.com>
2025-10-26 22:09:55 +0200

    Fix pycodestyle errors in 2-convolve_grayscale_padding.py

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -17,16 +17,17 @@ def convolve_grayscale_padding(images, kernel, padding):
     m, h, w = images.shape
     kh, kw = kernel.shape
     ph, pw = padding
-    
+
     padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
-    
+
     output_h = h + 2 * ph - kh + 1
     output_w = w + 2 * pw - kw + 1
-    
+
     convolved = np.zeros((m, output_h, output_w))
-    
+
     for i in range(output_h):
         for j in range(output_w):
-            convolved[:, i, j] = (padded[:, i:i+kh, j:j+kw] * kernel).sum(axis=(1, 2))
-    
+            convolved[:, i, j] = (padded[:, i:i+kh, j:j+kw] *
+                                  kernel).sum(axis=(1, 2))
+
     return convolved
