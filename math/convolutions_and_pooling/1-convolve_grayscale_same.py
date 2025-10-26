59417bda0e1ef1f40ee01dc569c2eb39c0db022c
JokMaker <j.kur@alustudent.com>
2025-10-26 22:12:24 +0200

    Fix pycodestyle errors in 1-convolve_grayscale_same.py

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -15,16 +15,18 @@ def convolve_grayscale_same(images, kernel):
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    
+
     pad_h = kh // 2
     pad_w = kw // 2
-    
-    padded = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
-    
+
+    padded = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+                    mode='constant')
+
     convolved = np.zeros((m, h, w))
-    
+
     for i in range(h):
         for j in range(w):
-            convolved[:, i, j] = (padded[:, i:i+kh, j:j+kw] * kernel).sum(axis=(1, 2))
-    
+            convolved[:, i, j] = (padded[:, i:i+kh, j:j+kw] *
+                                  kernel).sum(axis=(1, 2))
+
     return convolved
