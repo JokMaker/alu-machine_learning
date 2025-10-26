Fix same padding calculation in 3-convolve_grayscale

math/convolutions_and_pooling/5-convolve.py
@@ -1,42 +1,70 @@
 #!/usr/bin/env python3
-"""Module for convolution with multiple kernels"""
+'''
+    a function def
+    convolve(images, kernels, padding='same', stride=(1, 1)):
+    that performs a convolution on images using multiple kernels:
+'''
+
+
 import numpy as np
 
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """Performs convolution on images using multiple kernels
-
-    Args:
-        images: numpy.ndarray with shape (m, h, w, c) containing images
-        kernels: numpy.ndarray with shape (kh, kw, c, nc) containing kernels
-        padding: 'same', 'valid', or tuple of (ph, pw)
-        stride: tuple of (sh, sw) for stride
-
-    Returns:
-        numpy.ndarray containing the convolved images
     """
-    m, h, w, c = images.shape
-    kh, kw, _, nc = kernels.shape
+    Performs a convolution on images with multiple channels
+    using given padding and stride
+
+    parameters:
+        images [numpy.ndarray with shape (m, h, w, c)]:
+            contains multiple images
+            m: number of images
+            h: height in pixels of all images
+            w: width in pixels of all images
+            c: number of channels in the image
+        kernel [numpy.ndarray with shape (kh, kw, c)]:
+            contains the kernel for the convolution
+            kh: height of the kernel
+            kw: width of the kernel
+            c: number of channels in the image
+            nc:  is the number of kernels
+        padding [tuple of (ph, pw) or 'same' or 'valid']:
+            ph: padding for the height of the images
+            pw: padding for the width of the images
+            'same' performs same convolution
+            'valid' performs valid convolution
+        stride [tuple of (sh, sw)]:
+            sh: stride for the height of the image
+            sw: stride for the width of the image
+
+    if needed, images should be padded with 0s
+    function may only use two for loops maximum and no other loops are allowed
+
+    returns:
+        numpy.ndarray contained convolved images
+    """
+    m, height, width, c = images.shape
+    kh, kw, kc, nc = kernels.shape
     sh, sw = stride
-    
-    if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
-    elif padding == 'valid':
-        ph = pw = 0
+    if padding is 'same':
+        ph = ((((height - 1) * sh) + kh - height) // 2) + 1
+        pw = ((((width - 1) * sw) + kw - width) // 2) + 1
+    elif padding is 'valid':
+        ph = 0
+        pw = 0
     else:
         ph, pw = padding
-    
-    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
-    
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
-    
-    convolved = np.zeros((m, output_h, output_w, nc))
-    
-    for i in range(output_h):
-        for j in range(output_w):
-            for k in range(nc):
-                convolved[:, i, j, k] = (padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :] * kernels[:, :, :, k]).sum(axis=(1, 2, 3))
-    
-    return convolved
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    'constant', constant_values=0)
+    ch = ((height + (2 * ph) - kh) // sh) + 1
+    cw = ((width + (2 * pw) - kw) // sw) + 1
+    convoluted = np.zeros((m, ch, cw, nc))
+
+    for index in range(nc):
+        kernel_index = kernels[:, :, :, index]
+        for i, h in enumerate(range(0, (height + (2 * ph) - kh + 1), sh)):
+            for j, w in enumerate(range(0, (width + (2 * pw) - kw + 1), sw)):
+                output = np.sum(images[:, h: h + kh, w: w + kw, :]
+                                * kernel_index, axis=(1, 2, 3))
+                convoluted[:, i, j, index] = output
+
+    return convoluted