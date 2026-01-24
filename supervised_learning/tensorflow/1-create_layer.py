366a9937c65763cff09192e16b3e4addfcdce75f
JokMaker <j.kur@alustudent.com>
2026-01-24 16:31:12 +0200

    Simplify docstring in 1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -4,19 +4,8 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    """
-    Creates a layer for a neural network
-
-    Args:
-        prev: tensor output of the previous layer
-        n: number of nodes in the layer to create
-        activation: activation function that the layer should use
-
-    Returns:
-        tensor output of the layer
-    """
+    """Creates a layer for neural network"""
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init,
-                            name='layer')
+                            kernel_initializer=init, name='layer')
     return layer
