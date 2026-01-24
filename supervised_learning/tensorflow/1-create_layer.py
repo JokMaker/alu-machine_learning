af1c57cf088f11748c6889c414259b0b0001ab79
JokMaker <j.kur@alustudent.com>
2026-01-24 15:50:22 +0200

    Fix pycodestyle issues in 1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation):
     """Creates a layer for neural network"""
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.dense(prev, n, activation=activation, 
-                           kernel_initializer=initializer, name='layer')
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(prev, n, activation=activation,
+                            kernel_initializer=init, name='layer')
     return layer
