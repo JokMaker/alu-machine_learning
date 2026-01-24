9b4ca1041ec1ab9d11882b93a46188b3fe40a372
JokMaker <j.kur@alustudent.com>
2026-01-24 23:30:55 +0200

    Revert TF2 compatibility changes for TensorFlow task 2

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -3,15 +3,6 @@
 import tensorflow as tf
 
 
-# Support TF2 environments used by some checkers
-if hasattr(tf, 'compat') and hasattr(tf.compat, 'v1'):
-    try:
-        tf.compat.v1.disable_eager_execution()
-    except Exception:
-        pass
-    tf = tf.compat.v1
-
-
 def create_placeholders(nx, classes):
     """Creates placeholders for neural network"""
     x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -4,14 +4,6 @@ import tensorflow as tf
 create_layer = __import__('1-create_layer').create_layer
 
 
-# Support TF2 environments used by some checkers
-if hasattr(tf, 'compat') and hasattr(tf.compat, 'v1'):
-    try:
-        tf.compat.v1.disable_eager_execution()
-    except Exception:
-        pass
-
-
 def forward_prop(x, layer_sizes=[], activations=[]):
     """Creates the forward propagation graph for the neural network.
 
