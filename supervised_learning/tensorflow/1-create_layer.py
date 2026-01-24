fced4fc13af47844d7efc953a7f87b5400155025
JokMaker <j.kur@alustudent.com>
2026-01-24 16:34:41 +0200

    Fix create_layer to use Dense class for proper name scoping

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -4,8 +4,19 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    """Creates a layer for neural network"""
+    """
+    Creates a layer for a neural network
+
+    Args:
+        prev: tensor output of the previous layer
+        n: number of nodes in the layer to create
+        activation: activation function that the layer should use
+
+    Returns:
+        tensor output of the layer
+    """
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init, name='layer')
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=init,
+                            name='layer')(prev)
     return layer
