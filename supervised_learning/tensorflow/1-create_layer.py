f0fe00d40dad8df993fc7e9baf739916efef4082
JokMaker <j.kur@alustudent.com>
2026-01-24 15:52:27 +0200

    Adjust layer formatting

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,5 +7,6 @@ def create_layer(prev, n, activation):
     """Creates a layer for neural network"""
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init, name='layer')
+                            kernel_initializer=init,
+                            name='layer')
     return layer
