b90bf9719ec92474748364a6a65a0b037597aed6
JokMaker <j.kur@alustudent.com>
2026-01-24 18:35:38 +0200

    Tighten forward_prop loop to zip layer_sizes/activations

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -5,8 +5,7 @@ create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
-    """
-    Creates the forward propagation graph for the neural network
+    """Creates the forward propagation graph for the neural network.
 
     Args:
         x: placeholder for the input data
@@ -16,7 +15,7 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     Returns:
         prediction of the network in tensor form
     """
-    layer = x
-    for i in range(len(layer_sizes)):
-        layer = create_layer(layer, layer_sizes[i], activations[i])
-    return layer
+    output = x
+    for n, activation in zip(layer_sizes, activations):
+        output = create_layer(output, n, activation)
+    return output
